c20b94253c95f452fba086952dec73bfe99143e4
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-06-23 19:53:47 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+let button1 = document.querySelector('[type="button1"]'); 
+
+let gotonextFunction = () =>{
+    if (button1) {
+        button1.addEventListener('click', () =>
+            window.location.href = "2022-2023.html")
+    }
+}; 
+
+gotonextFunction(); 
+
+let button2 = document.querySelector('[type="button2"]'); 
+
+let gotonext2Function = () =>{
+    if (button2) {
+        button2.addEventListener('click', () =>
+            window.location.href = "2023-2024.html")
+    }
+}; 
+
+gotonext2Function(); 
+
+let button3 = document.querySelector('[type="button3"]'); 
+
+let gotonext3Function = () =>{
+    if (button3) {
+        button3.addEventListener('click', () =>
+            window.location.href = "2024-2025.html")
+    }
+}; 
+
+gotonext3Function(); 
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
