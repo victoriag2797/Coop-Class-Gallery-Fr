debbeb82b2854dfc5235d01c1caf5f940709cfba
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-06-24 00:11:16 +0000

    last commit

--- script.js
+++ script.js
@@ -31,6 +31,20 @@ let gotonext3Function = () =>{
 
 gotonext3Function(); 
 
+let button4 = document.querySelector('[type="button4"]'); 
+
+let randomFunct = () =>{
+    if (button4) {
+        button4.addEventListener('click', () => {
+            const pages = ["2022-2023.html", "2023-2024.html", "2024-2025.html"]
+            const randomIndex = Math.floor(Math.random() * pages.length);
+            window.location.href = pages[randomIndex];
+        });
+    }
+}; 
+
+randomFunct();
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -51,4 +65,25 @@ function showSlides(n) {
     slides[i].style.display = "none";  
   }
   slides[slideIndex-1].style.display = "block";  
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
